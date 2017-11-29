5eb5a7961dd8dff0208a6c0f80c7eb405c655483
naomi1234 <naomiyg@gmail.com>
2017-11-29 15:45:25 +0200

    basics

---
+++ project/app/src/main/java/com/example/tia29/geometry/Entites/Item.java
@@ -0,0 +1,10 @@
+package com.example.tia29.geometry.Entites;
+
+
+public interface Item {
+
+    public  String GetName();
+    public  int getId();
+
+
+ }
---
+++ project/app/src/main/java/com/example/tia29/geometry/UI/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.tia29.geometry.UI;
+
+import com.example.tia29.geometry.R;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity  {
+   
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start();
+    }
+
+
+    //new problem-new Exercise
+    public void newProb(View view) {
+        start();
+    }
+
+    //starts a new Exercise
+    public void start() {
+
+    }
+
+
+
+}
